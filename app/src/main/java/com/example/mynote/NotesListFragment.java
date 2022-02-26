d73f7dd4c07b3d780ee927baab2769090027c6dd
kikichechka <kikichechka@mail.ru>
2022-02-26 13:42:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynote/NotesListFragment.java
@@ -0,0 +1,12 @@
+package com.example.mynote;
+
+import androidx.fragment.app.Fragment;
+
+public class NotesListFragment extends Fragment {
+
+    
+
+    public static NotesListFragment newInstance() {
+        return new NotesListFragment();
+    }
+}
